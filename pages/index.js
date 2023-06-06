416dcffcb1e1b064ea4994c88368f45c6c8d37c2
Eduno <officialeduno@gmail.com>
2023-06-07 03:37:37 +0530

    Header Modified

--- pages/index.js
+++ pages/index.js
@@ -33,37 +33,29 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/x-icon" href="logo-wbg-icon.ico"></link>
       </Head>
-      
-      <section className="headerbg md:pt-24">
 
-        <ToastContainer />
-        <img src="eduno-white.png" className='md:h-20 mx-auto' alt="" />
+      <section className="headerbg md:pt-32 h-[80vh] flex justify-between">
 
-        <div className="py-2 px-4 mx-auto max-w-screen-xl text-center lg:py-8 lg:px-12">
+        <div className='ml-16'>
 
-          <Link href="https://drive.google.com/file/d/1ggceuBmqModIkmCPoDvozN0hZqQzFdkC/view?usp=sharing" target='_blank' className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
-            <span className="text-xs bg-[#616161] rounded-full text-white px-4 py-1.5 mr-3">New</span> <span className="text-sm font-medium">Eduno is out! See whats new</span>
-            <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
-          </Link>
+            <h1 className="mb-4 ml-1 text-4xl font-sans-regular text-left text-gray-500 md:text-6xl dark:text-white">Empower yourself 
+            with </h1> <img src="eduno-white.png" className='h-20 mt-8' alt="" />
 
-          <h1 className="mb-4 text-4xl font-sans-regular tracking-tight leading-none text-gray-500 md:text-5xl lg:text-6xl dark:text-white">Empower yourself with Eduno</h1>
+            <div className="flex flex-col ml-2 mt-8 mb-8 md:mb-16 space-y-4 sm:flex-row sm:justify-left sm:space-y-0 sm:space-x-4">
+              <Link href="https://drive.google.com/file/d/1ggceuBmqModIkmCPoDvozN0hZqQzFdkC/view?usp=sharing" target='_blank' className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-[#5d5d5d] hover:bg-gray-700 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                Learn more
+                <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+              </Link>
 
-          <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Eduno is a cutting-edge platform that offers a wide range of technical education courses, including Python programming, web development, Android app development, and more. The platform is designed to provide an engaging and interactive learning experience for students of all levels, from beginners to advanced learners.</p>
-
-          <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-
-            <Link href="https://drive.google.com/file/d/1ggceuBmqModIkmCPoDvozN0hZqQzFdkC/view?usp=sharing" target='_blank' className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-[#5d5d5d] hover:bg-gray-700 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-              Learn more
-              <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-            </Link>
-
-            <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-              <svg className="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
-              Watch video
-            </a>
+              <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                <svg className="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
+                Watch video
+              </a>
+            </div>
 
           </div>
-
+        <div className='mr-24'>
+          <img src="./img/handcode.png" className='md:h-[50vh]' alt="" />
         </div>
 
       </section>
