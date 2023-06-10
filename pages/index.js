2a72905ecd02363061e291d8dadbfa13b7b90a8f
Official Eduno <officialeduno@gmail.com>
2023-06-10 23:11:06 +0530

    Course Page Design (Training Page)

--- pages/index.js
+++ pages/index.js
@@ -109,7 +109,7 @@ export default function Home() {
 
             <div class="flex items-start justify-between gap-2 px-2">
               <div class="flex flex-col">
-                <a href="#" class="text-lg font-bold text-[#EEEEEE] transition duration-100 hover:text-gray-500 lg:text-xl">Training & Intership 2023</a>
+                <Link href={"/courses/training"} class="text-lg font-bold text-[#EEEEEE] transition duration-100 hover:text-[#FD7014] lg:text-xl">Training & Intership 2023</Link>
                 <span class="text-gray-500">by EDUNO</span>
               </div>
 
@@ -131,7 +131,7 @@ export default function Home() {
 
             <div class="flex items-start justify-between gap-2 px-2">
               <div class="flex flex-col">
-                <a href="#" class="text-lg font-bold text-[#EEEEEE] transition duration-100 hover:text-gray-500 lg:text-xl">Data Science</a>
+                <Link href={"/courses/datascience"} class="text-lg font-bold text-[#EEEEEE] transition duration-100 hover:text-[#FD7014] lg:text-xl">Data Science</Link>
                 <span class="text-gray-500">by EDUNO</span>
               </div>
 
@@ -153,7 +153,7 @@ export default function Home() {
 
             <div class="flex items-start justify-between gap-2 px-2">
               <div class="flex flex-col">
-                <a href="#" class="text-lg font-bold text-[#EEEEEE] transition duration-100 hover:text-gray-500 lg:text-xl">AI & ML</a>
+                <Link href={"/courses/aiml"} class="text-lg font-bold text-[#EEEEEE] transition duration-100 hover:text-[#FD7014] lg:text-xl">AI & ML</Link>
                 <span class="text-gray-500">by EDUNO</span>
               </div>
 
@@ -171,17 +171,17 @@ export default function Home() {
         <div className='flex flex-row gap-4 md:gap-8 md:justify-center py-4 md:py-8 overflow-x-auto scroll whitespace-nowrap scroll-smooth w-[90vw]'>
 
           <div>
-            <a href="#" class="group relative mb-2 w-72 md:w-96 h-40 md:h-80 block overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
+            <Link href={"/courses/android"} class="group relative mb-2 w-72 md:w-96 h-40 md:h-80 block overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
               <img src="/img/android.png" loading="lazy" alt="Photo by Austin Wade" class="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
 
               <div class="absolute left-0 bottom-2 flex gap-2">
                 <span class="rounded-r-lg bg-red-500 px-3 py-1.5 text-sm font-semibold uppercase tracking-wider text-white">-50%</span>
               </div>
-            </a>
+            </Link>
 
             <div class="flex items-start justify-between gap-2 px-2">
               <div class="flex flex-col">
-                <a href="#" class="text-lg font-bold text-[#EEEEEE] transition duration-100 hover:text-gray-500 lg:text-xl">Android App Development</a>
+                <Link href={"/courses/android"} class="text-lg font-bold text-[#EEEEEE] transition duration-100 hover:text-[#FD7014] lg:text-xl">Android App Development</Link>
                 <span class="text-gray-500">by EDUNO</span>
               </div>
 
@@ -192,17 +192,17 @@ export default function Home() {
             </div>
           </div>
           <div>
-            <a href="#" class="group relative mb-2 w-72 md:w-96 h-40 md:h-80 block overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
+            <Link href={"/courses/fullstack"} class="group relative mb-2 w-72 md:w-96 h-40 md:h-80 block overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
               <img src="/img/webdev2.png" loading="lazy" alt="Photo by Austin Wade" class="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
 
               <div class="absolute left-0 bottom-2 flex gap-2">
                 <span class="rounded-r-lg bg-red-500 px-3 py-1.5 text-sm font-semibold uppercase tracking-wider text-white">-50%</span>
               </div>
-            </a>
+            </Link>
 
             <div class="flex items-start justify-between gap-2 px-2">
               <div class="flex flex-col">
-                <a href="#" class="text-lg font-bold text-[#EEEEEE] transition duration-100 hover:text-gray-500 lg:text-xl">Full Stack Web Development</a>
+                <Link href={"/courses/fullstack"} class="text-lg font-bold text-[#EEEEEE] transition duration-100 hover:text-[#FD7014] lg:text-xl">Full Stack Web Development</Link>
                 <span class="text-gray-500">by EDUNO</span>
               </div>
 
@@ -213,17 +213,17 @@ export default function Home() {
             </div>
           </div>
           <div>
-            <a href="#" class="group relative mb-2 w-72 md:w-96 h-40 md:h-80 block  overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
+            <Link href={"/courses/java"} class="group relative mb-2 w-72 md:w-96 h-40 md:h-80 block  overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
               <img src="/img/java.png" loading="lazy" alt="Photo by Austin Wade" class="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
 
               <div class="absolute left-0 bottom-2 flex gap-2">
                 <span class="rounded-r-lg bg-red-500 px-3 py-1.5 text-sm font-semibold uppercase tracking-wider text-white">-50%</span>
               </div>
-            </a>
+            </Link>
 
             <div class="flex items-start justify-between gap-2 px-2">
               <div class="flex flex-col">
-                <a href="#" class="text-lg font-bold text-[#EEEEEE] transition duration-100 hover:text-gray-500 lg:text-xl">Java with DSA</a>
+                <Link href={"/courses/java"} class="text-lg font-bold text-[#EEEEEE] transition duration-100 hover:text-[#FD7014] lg:text-xl">Java with DSA</Link>
                 <span class="text-gray-500">by EDUNO</span>
               </div>
 
