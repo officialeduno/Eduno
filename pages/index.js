0611e511ba0807b221e87a3dabc6e93301a38c34
Official Eduno <officialeduno@gmail.com>
2023-06-17 01:28:39 +0530

    Watch Video Link update

--- pages/index.js
+++ pages/index.js
@@ -30,10 +30,10 @@ export default function Home() {
               <svg className="w-5 ml-2 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
             </Link>
 
-            <a href="#" className="inline-flex justify-center items-center px-2 py-2 ml-2 md:ml-4 md:py-2 md:px-5 text-base text-font text-center text-[#FD7014] rounded-lg border border-[#EEEEEE]">
+            <Link href={"https://www.youtube.com/watch?v=tJBoxZLmUCY"} target='_blank' className="inline-flex justify-center items-center px-2 py-2 ml-2 md:ml-4 md:py-2 md:px-5 text-base text-font text-center text-[#FD7014] rounded-lg border border-[#EEEEEE]">
               <svg className="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
               Watch video
-            </a>
+            </Link>
           </div>
 
         </div>
